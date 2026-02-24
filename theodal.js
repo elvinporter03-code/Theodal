d47a84c601f38647598d73c19bd89a2887502070
elvinporter03-code <elvinporter03@gmail.com>
2026-02-24 17:34:06 +0100

    snyggat till sidan lite och börjat migrera knapparna till konstanter för lättare frontendgrejer

--- theodal.js
+++ theodal.js
@@ -1,7 +1,36 @@
 "use strict";
-var Albanian_bartender = './music/albanian_music/Albanian Bartender.mp3';
+var current_song;
+//knappar:
+var Albanian_Bartender = document.getElementById("Albanian_Bartender");
+var playbtn = document.getElementById("Play_Pause");
 function play_song(path) {
     var song = new Audio(path);
-    song.play();
+    if (current_song != song) {
+        current_song = song;
+        current_song.play();
+    }
 }
 ;
+function Play_Pause() {
+    if (current_song.paused) {
+        current_song.play();
+    }
+    else {
+        current_song.pause();
+    }
+}
+if (Albanian_Bartender !== null) {
+    Albanian_Bartender.addEventListener("click", function () { play_song('./music/albanian_music/Albanian Bartender.mp3'); });
+}
+if (playbtn !== null) {
+    playbtn.addEventListener("click", function () {
+        Play_Pause();
+        if (current_song.paused) {
+            playbtn.textContent = "PLAY";
+        }
+        else {
+            playbtn.textContent = "PAUSE";
+        }
+        ;
+    });
+}
--- theodal.ts
+++ theodal.ts
@@ -1,7 +1,34 @@
 type Song = HTMLAudioElement;
-const Albanian_bartender : string = './music/albanian_music/Albanian Bartender.mp3';
-
+let current_song: HTMLAudioElement;
+//knappar:
+const Albanian_Bartender : HTMLElement | null = document.getElementById("Albanian_Bartender");
+const playbtn : HTMLElement | null = document.getElementById("Play_Pause");
 function play_song(path: string): void{
-    const song: Song = new Audio(path);
-    song.play();
+    let song: Song = new Audio(path);
+    if(current_song != song){
+        current_song= song;
+        current_song.play();
+    } 
 };
+
+function Play_Pause(){
+    if(current_song.paused){
+       current_song.play();
+    } else{
+       current_song.pause();
+    }
+}
+
+if(Albanian_Bartender !== null) {
+    Albanian_Bartender.addEventListener("click", () => 
+        {play_song('./music/albanian_music/Albanian Bartender.mp3');});
+}
+if(playbtn !== null) {
+    playbtn.addEventListener("click", () => 
+        {Play_Pause();
+        if(current_song.paused){playbtn.textContent="PLAY";}
+        else{
+            playbtn.textContent="PAUSE";
+        };
+        });
+}
