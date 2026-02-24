84c4767c4cdb293602b005b1f62a1b4a95c92d1b
elvinporter03-code <elvinporter03@gmail.com>
2026-02-24 21:50:17 +0100

    snyggare layout, artistivew implementerad, förbättrad backendlogik, och bugfixar runt uppspelningen

--- theodal.js
+++ theodal.js
@@ -1,13 +1,25 @@
 "use strict";
 var current_song;
 //knappar:
-var Albanian_Bartender = document.getElementById("Albanian_Bartender");
 var playbtn = document.getElementById("Play_Pause");
-function play_song(path) {
-    var song = new Audio(path);
+var previousbtn = document.getElementById("Previous");
+var skipbtn = document.getElementById("Skip");
+var albantheobtn = document.getElementById("albantheo");
+var albanmusik = document.getElementById("albanmusik");
+var Albanian_Bartenderbtn = document.getElementById("Albanian_Bartender");
+var Albanian_Bartender_Audio = new Audio('./music/albanian_music/Albanian Bartender.mp3');
+function play_song(song) {
     if (current_song != song) {
         current_song = song;
         current_song.play();
+        if (playbtn !== null) {
+            if (current_song.paused) {
+                playbtn.textContent = "PLAY";
+            }
+            else {
+                playbtn.textContent = "PAUSE";
+            }
+        }
     }
 }
 ;
@@ -19,8 +31,20 @@ function Play_Pause() {
         current_song.pause();
     }
 }
-if (Albanian_Bartender !== null) {
-    Albanian_Bartender.addEventListener("click", function () { play_song('./music/albanian_music/Albanian Bartender.mp3'); });
+function skip() {
+}
+function previous() {
+}
+function expand_artist(artist) {
+    if (artist.style.visibility === "visible") {
+        artist.style.visibility = "hidden";
+    }
+    else {
+        artist.style.visibility = "visible";
+    }
+}
+if (Albanian_Bartenderbtn !== null) {
+    Albanian_Bartenderbtn.addEventListener("click", function () { play_song(Albanian_Bartender_Audio); });
 }
 if (playbtn !== null) {
     playbtn.addEventListener("click", function () {
@@ -33,4 +57,13 @@ if (playbtn !== null) {
         }
         ;
     });
+}
+if (previousbtn !== null) {
+    previousbtn.addEventListener("click", function () { previous(); });
+}
+if (skipbtn !== null) {
+    skipbtn.addEventListener("click", function () { skip(); });
+}
+if (albantheobtn !== null && albanmusik !== null) {
+    albantheobtn.addEventListener("click", function () { expand_artist(albanmusik); });
 }
--- theodal.ts
+++ theodal.ts
@@ -1,18 +1,31 @@
 type Song = HTMLAudioElement;
 let current_song: HTMLAudioElement;
 //knappar:
-const Albanian_Bartender : HTMLElement | null = document.getElementById("Albanian_Bartender");
-const playbtn : HTMLElement | null = document.getElementById("Play_Pause");
 
-function play_song(path: string): void{
-    let song: Song = new Audio(path);
+const playbtn : HTMLElement | null = document.getElementById("Play_Pause");
+const previousbtn : HTMLElement | null = document.getElementById("Previous");
+const skipbtn : HTMLElement | null = document.getElementById("Skip");
+
+const albantheobtn : HTMLElement | null = document.getElementById("albantheo");
+const albanmusik :  HTMLElement | null = document.getElementById("albanmusik"); 
+const Albanian_Bartenderbtn : HTMLElement | null = document.getElementById("Albanian_Bartender");
+const Albanian_Bartender_Audio: Song = new Audio('./music/albanian_music/Albanian Bartender.mp3');
+
+function play_song(song: Song): void{
     if(current_song != song){
         current_song= song;
         current_song.play();
+        if(playbtn !== null){
+            if(current_song.paused){
+                playbtn.textContent="PLAY";
+            }   else{
+            playbtn.textContent="PAUSE";
+            }
+        }
     } 
 };
 
-function Play_Pause(){
+function Play_Pause():void{
     if(current_song.paused){
        current_song.play();
     } else{
@@ -20,16 +33,45 @@ function Play_Pause(){
     }
 }
 
-if(Albanian_Bartender !== null) {
-    Albanian_Bartender.addEventListener("click", () => 
-        {play_song('./music/albanian_music/Albanian Bartender.mp3');});
+
+function skip():void{
+
+}
+function previous():void{
+
+}
+function expand_artist(artist : HTMLElement) : void{
+    if(artist.style.visibility === "visible"){
+        artist.style.visibility = "hidden";
+    }
+    else {
+        artist.style.visibility = "visible";
+    }
+
+}
+
+if(Albanian_Bartenderbtn !== null) {
+    Albanian_Bartenderbtn.addEventListener("click", () => 
+        {play_song(Albanian_Bartender_Audio);});
 }
 if(playbtn !== null) {
     playbtn.addEventListener("click", () => 
         {Play_Pause();
-        if(current_song.paused){playbtn.textContent="▶";}
+        if(current_song.paused){playbtn.textContent="PLAY";}
         else{
-            playbtn.textContent="II";
+            playbtn.textContent="PAUSE";
         };
-        });
+    });
+}
+if(previousbtn !== null) {
+    previousbtn.addEventListener("click", () => 
+        {previous()});
+}
+if(skipbtn !== null) {
+    skipbtn.addEventListener("click", () => 
+        {skip()});
+}
+if(albantheobtn !== null && albanmusik !== null){
+    albantheobtn.addEventListener("click", () => 
+    {expand_artist(albanmusik)})
 }
